ccb185760693517f2578712704bc735fb4adc78c
Christopher Snow <csnow@live.ca>
2020-06-12 18:43:03 -0400

    Allow a secondary downloads path.

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ app.use('/static', express.static(
   path.join(__dirname, 'public'),
   {maxAge: 86400000}
 ));
+app.use('/downloads', express.static(
+  path.join(__dirname, 'public/downloads'),
+  {maxAge: 86400000}
+));
 
 
 app.use('/', indexRouter);
@@ -67,7 +71,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
-  console.error(err);
+  //console.error(err);
   res.status(err.status || 500).render('error');
 });
 
