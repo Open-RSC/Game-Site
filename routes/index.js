a100dfdb1dd458391f0f39214423974ff3a5ef6f
Suli <suli@runescapeclassic.dev>
2020-10-05 23:30:06 -0400

    Soft crash with invalid patch notes json file

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,45 @@
-const notes = require('../data/PatchNotes.json');
+let notes;
+try {
+  notes = require('../data/PatchNotes.json');
+}
+catch (err) {
+  console.error("Could not parse PatchNotes.json file - please correct!");
+}
+
+//const notes = require('../data/PatchNotes.json');
 
 const express = require('express');
 const router = express.Router();
 const db = require('../db')
 
 router.get('/', async (req, res, next) => {
-    const servers = await db.getOnline();
-    const feed = await db.getLiveFeeds();
-    let online = 0;
-    if (!isNaN(servers.openrsc) && !isNaN(servers.cabbage)) {
-        online = servers.openrsc + servers.cabbage;
+  const servers = await db.getOnline();
+  const feed = await db.getLiveFeeds();
+  let online = 0;
+  if (!isNaN(servers.openrsc) && !isNaN(servers.cabbage)) {
+    online = servers.openrsc + servers.cabbage;
+  }
+  else if (!isNaN(servers.openrsc)) {
+    online = servers.openrsc;
+  }
+  else if (!isNaN(servers.cabbage)) {
+    online = servers.cabbage;
+  }
+  let onlineString = "";
+  if (online !== 0) {
+    if (online === 1) {
+      onlineString = "There is currently " + online + " person playing!";
     }
-    else if (!isNaN(servers.openrsc)) {
-        online = servers.openrsc;
+    else {
+      onlineString = "There are currently " + online + " people playing!";
     }
-    else if (!isNaN(servers.cabbage)) {
-        online = servers.cabbage;
-    }
-    let onlineString = "";
-    if (online !== 0) {
-      if (online === 1) {
-        onlineString = "There is currently " + online + " person playing!";
-      }
-      else {
-        onlineString = "There are currently " + online + " people playing!";
-      }
-    }
-    res.render('index', {
-      page_name: "Open RuneScape Classic - Play the replica today",
-      online: onlineString,
-      notes: notes.notes,
-      live_feed: feed
-    });
+  }
+  res.render('index', {
+    page_name: "Open RuneScape Classic - Play the replica today",
+    online: onlineString,
+    notes: notes ? notes.notes : [],
+    live_feed: feed
+  });
 });
 
 module.exports = router;
--- routes/patchnotes.js
+++ routes/patchnotes.js
@@ -1,4 +1,11 @@
-const notes = require('../data/PatchNotes.json');
+try {
+  notes = require('../data/PatchNotes.json');
+}
+catch (err) {
+  console.error("Could not parse PatchNotes.json file - please correct!");
+}
+
+//const notes = require('../data/PatchNotes.json');
 
 const express = require('express');
 const router = express.Router();
@@ -7,7 +14,7 @@ router.get('/', (req, res, next) => {
   res.render('patchnotes', {
     page_name: "Open RuneScape Classic | Patch Notes",
     description: "The most recent patch notes on the Open RuneScape Classic source!",
-    notes: notes.notes
+    notes: notes ? notes.notes : []
   });
 });
 
